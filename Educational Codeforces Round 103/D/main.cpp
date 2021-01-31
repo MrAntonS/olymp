418eeabeb0d67a7a66e28cca06c326f3482b3f73
MrAntonS <saenkoai@mail.ru>
2021-01-31 22:02:41 +0300

    '[

--- Educational Codeforces Round 103/D/main.cpp
+++ Educational Codeforces Round 103/D/main.cpp
@@ -4,6 +4,16 @@
 #define IO  ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
 using namespace std;
 
+void dfs(vector<vector<int>> &a, vector<int> c, int b) {
+    c[b] = 1;
+    for (auto &i : a[b]) {
+        if (c[i] == 1) cout << "Cycle" << endl;
+        else dfs(a, c, i);
+    }
+    c[b] = 0;
+    return;
+}
+
 void solve() {
     
 }
