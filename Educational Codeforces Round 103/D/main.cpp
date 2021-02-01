0ee41f9f5997a10bb3955b1b019a9448342067de
MrAntonS <saenkoai@mail.ru>
2021-02-01 16:28:12 +0300

    YESSSS

--- Educational Codeforces Round 103/D/main.cpp
+++ Educational Codeforces Round 103/D/main.cpp
@@ -4,30 +4,38 @@
 #define IO  ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
 using namespace std;
 
-int dfs(vector<vector<int>> &a, vector<vector<int>> &a1, int last, int b, bool step, int cnt) {
-    if (step) {
-        for (auto &i : a[b]) {
-            if (last == i) continue;
-            else {
-                cnt = dfs(a, a1, b, i, !step, cnt + 1);
-            }
-        }
-    } else {
-        for (auto &i : a1[b]) {
-            if (last == i) continue;
-             else {
-                cnt = dfs(a, a1, b, i, !step, cnt + 1);
-            }
-        }
-    }
-    return cnt;
-}
+// int dfs(vector<vector<int>> &a, vector<vector<int>> &a1, int last, int b, bool step, int cnt) {
+//     if (res[b]) return res[b];
+//     if (step) {
+//         for (auto &i : a[b]) {
+//             if (res[b]) return res[b];
+//             if (last == i) continue;
+//             else {
+//                 cnt = dfs(a, a1, b, i, !step, cnt + 1);
+//             }
+//         }
+//     } else {
+//         for (auto &i : a1[b]) {
+//             if (res[b]) return res[b];
+//             if (last == i) continue;
+//              else {
+//                  if (res[i]) {
+//                     res[b] = res[i];
+//                     break;
+//                 }
+//                 cnt = dfs(a, a1, b, i, !step, cnt + 1);
+//             }
+//         }
+//     }
+//     return cnt;
+// }
 
 void solve() {
     int n;
     cin >> n;
     vector<vector<int>> a(n + 1);
     vector<vector<int>> a1(n + 1);
+    vector<int> res(n + 1, 0);
     string s;
     cin >> s;
     // for (int i = 1; i <= n; i++) {
@@ -40,50 +48,53 @@ void solve() {
     //     }
     // }
     // for(int i = 0; i < n + 1; i++) {
-    //     cout << dfs(a, a1, -1, i, true, 1) << ' ';
+    //     res[i] = dfs(a, a1, -1, i, true, 1);
+    //     cout << res[i]  << ' ';
+    //     for (auto &i : res) {
+    //         cout << i << ' ';
+    //     }
+    //     cout << endl;
     // }
-    int c = 1;
-    bool bk = true;
-    for (int j = 0; j < n; j++) {
-        if (s[j] == 'R' && bk) {
-            c++;
-            bk = !bk;
-        } else if (s[j] == 'L' && !bk) {
-            c++;
-            bk = !bk;
-        } else {
-            break;
-        }
-    }
-    cout << c << ' ';
-    for (int i = 0; i < n; i++) {
-        bool bk = true, f = true;
+    for(int i = 0; i < n + 1; i++) {
+        bool chet = true;
+        int last = i;
         int cnt = 1;
-        for (int j = i; j >= 0; j--) {
-            if (s[j] == 'L' && bk) {
-                cnt++;
-                bk = !bk;
-            } else if (s[j] == 'R' && !bk) {
-                cnt++;
-                bk = !bk;
-            } else {
-                break;
+        if (res[i] <= 1) {
+            for (int j = i; j < n; j++) {
+                if (chet && s[j] == 'R') {
+                    cnt++;
+                    chet = !chet;
+                    last = j + 1;
+                    // if (j == 1) {
+                    //     cout << 'A' << endl;
+                    // }
+                } else if (!chet && s[j] == 'L') {
+                    cnt++;
+                    chet = !chet;
+                    last = j + 1;
+                    // if (j == 1) {
+                    //     cout << 'A' << endl;
+                    // }
+                } else break;
+            }
+            // if (i == 1) {
+            //     cout << cnt << endl;
+            // }
+            res[i] += cnt;
+            for (int j = i; j <= last; j++) {
+                if ((j - i) % 2 == 0) {
+                    res[j] = res[i];
+                } if (j + 1 > last && j + 1 < n + 1 && (j + 1 - i) % 2 && res[j + 1] < 1) {
+                    res[j + 1] += 1;
+                }
             }
         }
-        for (int j = i + 1; j < n; j++) {
-            if (s[j] == 'R' && f) {
-                cnt++;
-                f = !f;
-            } else if (s[j] == 'L' && !f) {
-                cnt++;
-                f = !f;
-            } else {
-             break;
-            }
-        }
-        cout << cnt << ' ';
     }
-    cout << endl;
+    for (auto &i : res) {
+                cout << i << ' ';
+            }
+            cout << endl;
+    
 }
 
 int main() {
